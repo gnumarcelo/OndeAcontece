cf3d7e1dc2afc8c2984a5c8f7e11a606627d1350
Marcelo Menezes <marcelomenezes@Marcelo-Menezess-MacBook.local>
2011-01-15 12:36:29 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OndeAcontece::Application.config.secret_token = '0932d9fdd95466942b5cbf26005a221ebbe937d1ead4eee43b58802b5bbdd2d7a541388c8c4795dad70415898874db3b738b2f9afbfb0a2cf27928b68b2c5bd7'
